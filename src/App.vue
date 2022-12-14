4bbe3fd8d3ed3dfca8be9098e5a469fd3dcbe820
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-14 15:47:58 +0100

    Creato il componente che conterrà la lista dei personaggi;

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import SelectCategory from './components/SelectCategory.vue';
+import CharacterList from './components/CharacterList.vue';
 
 export default {
     name: "App",
     components: {
         AppHeader,
         SelectCategory,
+        CharacterList,
     }
 }
 </script>
@@ -14,6 +16,7 @@ export default {
 <template>
     <AppHeader />
     <SelectCategory />
+    <CharacterList />
 </template>
 
 <style lang="scss">
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "CharacterList",
+}
+</script>
+
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.container {
+    background-color: $primaryColor;
+    margin-top: 30px;
+    padding: 30px;
+}
+</style>
