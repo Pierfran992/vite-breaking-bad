8debbf4b1c41ad299cd49b2cc2cdf94dd4c60f9a
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-14 14:46:40 +0100

    Componente AppHeader completato;

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,34 @@ export default {
 </script>
 
 <template>
-
+    <section>
+        <!-- logo del titolo -->
+        <div>
+            <img src="../assets/img/kisspng-logo-walter-white-vector-graphics-television-show-breaking-bad-png-18-5b754e10a05298.6389091215344143526567.png"
+                alt="Logo Breaking Bad">
+        </div>
+        <!-- Titolo della pagina -->
+        <h1>Breaking Bad Api</h1>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 @use '../styles/partials/mixins' as *;
+
+section {
+    height: 100px;
+    padding-left: 20px;
+    @include start();
+
+    div {
+        width: 100px;
+        height: 100%;
+        margin-right: 20px;
+    }
+
+    h1 {
+        font-size: 27px;
+    }
+}
 </style>
