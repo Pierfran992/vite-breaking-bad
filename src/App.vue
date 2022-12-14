f36585a2f23e88f42c8c72f85eab8f38c8e1de7d
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-14 14:29:50 +0100

    Richiamato nell'App il primo componente creato AppHeader;

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+    name: "App",
+    components: {
+        AppHeader,
+
+    }
+}
 </script>
 
 <template>
-
+    <AppHeader />
 </template>
 
 <style lang="scss">
-
+@use './styles/partials/variables' as *;
+@use './styles/general.scss' as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+</style>
