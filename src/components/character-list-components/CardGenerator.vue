a493403ba4c4266bd0a4f9205d9f32a0316c2d0f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-14 17:01:44 +0100

    Creato il nuovo componente per generare le card;

---
+++ src/components/character-list-components/CardGenerator.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "CardGenerator",
+
+    props: [],
+}
+</script>
+
+<template>
+    <!-- creo la card -->
+    <div class="gen_card">
+        <div class="slot_img_card">
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="">
+        </div>
+        <h4>Rick Sanchez</h4>
+        <span></span>
+        <br>
+        <span></span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+@use '../../styles/partials/mixins' as *;
+
+.gen_card {
+    background-color: $secondaryColor;
+    width: calc(100% / 5);
+    padding: 10px;
+    text-align: center;
+
+    .slot_img_card {
+        margin-bottom: 10px;
+    }
+
+}
+</style>
