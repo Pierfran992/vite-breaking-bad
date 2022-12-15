dc0faee7d9a5741343b88d03c2a8e67ad285f946
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-15 15:18:27 +0100

    Creata una nuova variabile nello store in cui verranno inseriti il totale dei charcter trovati;

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
+    totalResults: "",
     characterList: [],
     apiURL: "https://rickandmortyapi.com/api/character",
     searchStatus: "",
