c1a844422ab99459afcc800777b16b7120ea2dc9
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-12-15 13:45:31 +0100

    Richiamato lo store anche nel file principale App e portati i metodi dal componente figlio CharacterList al componente padre App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import SelectCategory from './components/SelectCategory.vue';
 import CharacterList from './components/CharacterList.vue';
+
+import { store } from './store.js';
 
 export default {
     name: "App",
@@ -9,6 +12,28 @@ export default {
         AppHeader,
         SelectCategory,
         CharacterList,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        // creo il metodo per far inserire gli oggetti contenuti il results nell'array vuoto characterList
+        getCharacters() {
+            axios
+                .get(store.apiURL)
+                .then(res => {
+                    store.characterList = res.data.results;
+                })
+                .catch(problem => {
+                    console.log("Errori", problem);
+                });
+        }
+    },
+    mounted() {
+        // faccio partire il metodo dopo che ha caricato la struttura della pagina
+        this.getCharacters();
     }
 }
 </script>
